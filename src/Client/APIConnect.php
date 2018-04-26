a668cab381f12462c204c79e3ac463e7fec0907e
jesseves <jsnyder@weta.org>
2018-04-26 11:47:12 -0400

    Move API connection into a service.

--- src/Client/APIConnect.php
+++ src/Client/APIConnect.php
@@ -6,13 +6,17 @@ use Drupal\pbs_media_manager\Client\PBS_Media_Manager_API_Client as PBSClient;
 class APIConnect {
   
   private $endpoint = "https://media.services.pbs.org/api/v1/";
+  protected $key;
+  protected $secret;
+  
+  public function __construct() {
+    $config = \Drupal::config('pbs_media_manager.settings');
+    $this->key = $config->get('key');
+    $this->secret = $config->get('secret');
+  }
   
   public function connect() {
-    //Immutable Config (Read Only)
-    $config = \Drupal::config('pbs_media_manager.settings');
-    $key = $config->get('key');
-    $secret = $config->get('secret');
-    return new PBSClient($key, $secret, $this->endpoint);
+    return new PBSClient($this->key, $this->secret, $this->endpoint);
   }
   
 }
